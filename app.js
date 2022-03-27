cb0a7a0325420c82d6114b4a5ec6bbceb81cda09
Christina Armand <christina@Christinas-MacBook-Air.local>
2022-03-27 12:18:18 -0400

    trouble with deplying heroku

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/failure', function (req, res) {
 
 });
 
-app.listen(proces.env.PORT  || 3000, function () {
+app.listen(process.env.PORT||3000, function() {
     console.log("server is running on port 3000")
 });
 
